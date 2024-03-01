0e408eb0532056b3243482b6b8f310ab48580b84
Martijn van den Berg <mvandenberg@laskar-puntlastechniek.nl>
2024-03-01 14:53:42 +0100

    Huiswerk les10 gemaakt

---
+++ presentatie.py
@@ -0,0 +1,9 @@
+def presenteer(my_dict):
+    for k, v in my_dict.items():
+        print(k, ":", v)
+    print("===========================")
+    totaal = sum(my_dict.values())
+    print ("Totaal : ", totaal) 
+
+
+
